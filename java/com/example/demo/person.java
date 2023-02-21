0e2021e80f64fd6691a3310cb77d7b963c7e0fe2
FredaFrancis <honeycathy14@gmail.com>
2023-02-21 09:10:04 +0530

    first program

---
+++ java/com/example/demo/person.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+public class Person {
+	  
+	  private int id;
+	  private String name;
+	  private int age;
+	  
+	  public int getId() {
+	    return id;
+	  }
+	  public void setId(int id) {
+	    this.id = id;
+	  }
+	  
+	  public Person(){
+	    
+	  }
+	  
+	  public Person(int id,String name, int age) {
+	    super();
+	    this.id=id;
+	    this.name = name;
+	    this.age = age;
+	  }
+	//getter and setter methods
+	}
